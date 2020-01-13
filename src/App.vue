6c1f5e5258d392f7187ad58eac05bbc7b603c5b7
nargok <nargok@gmail.com>
2020-01-13 15:42:55 +0900

    AppBar component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
+<div>
+  <AppBar />
   <router-view />
+ </div>
 </template>
 
 <script>
-
+import AppBar from "./components/AppBar.vue";
 export default {
+  
   name: 'App',
   components: {
+    AppBar
   },
 
   data: () => ({
---
+++ src/components/AppBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-app-bar
+      color="white"
+      dense
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-title>Camp Item</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+  <router-view />
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'AppBar',
+
+  data: () => ({
+  }),
+};
+</script>
