0a297c93439b240cfbc8bfce17cf729a8c677076
nargok <nargok@gmail.com>
2020-01-13 15:04:11 +0900

    install vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-card width="400px" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">定番ギア「XXXの焚火台」</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-img
+          :src="require('./assets/campfire-896196_640.jpg')"
+        >          
+        </v-img>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore iure laborum laudantium, pariatur soluta est cumque, reprehenderit veritatis blanditiis nisi, necessitatibus dolor eveniet doloribus explicabo magnam. Odit quasi consequuntur pariatur?</p>
+      </v-card-text>
+    </v-card>
+    <v-card width="400px" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">小型焚き火台XXXの威力</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-img
+          :src="require('./assets/fireplace-1598243_640.jpg')"
+        >          
+        </v-img>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore iure laborum laudantium, pariatur soluta est cumque, reprehenderit veritatis blanditiis nisi, necessitatibus dolor eveniet doloribus explicabo magnam. Odit quasi consequuntur pariatur?</p>
+      </v-card-text>
+    </v-card>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+  },
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
