66fe2e8af29561296e649bc100d6cf06552d1daa
nargok <nargok@gmail.com>
2020-01-13 15:11:18 +0900

    change router home

---
+++ src/views/CampItem.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <v-app>
+    <v-card width="400px" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">定番ギア「XXXの焚火台」</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-img
+          :src="require('../assets/campfire-896196_640.jpg')"
+        >          
+        </v-img>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore iure laborum laudantium, pariatur soluta est cumque, reprehenderit veritatis blanditiis nisi, necessitatibus dolor eveniet doloribus explicabo magnam. Odit quasi consequuntur pariatur?</p>
+      </v-card-text>
+    </v-card>
+    <v-card width="400px" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">小型焚き火台XXXの威力</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-img
+          :src="require('../assets/fireplace-1598243_640.jpg')"
+        >          
+        </v-img>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore iure laborum laudantium, pariatur soluta est cumque, reprehenderit veritatis blanditiis nisi, necessitatibus dolor eveniet doloribus explicabo magnam. Odit quasi consequuntur pariatur?</p>
+      </v-card-text>
+    </v-card>
+  </v-app>
+</template>
